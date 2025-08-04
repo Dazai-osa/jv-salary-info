138c663126a7d8ed6c788911f14b0463ff50bb80
Andrew <reznichenko.a200212@gmail.com>
2025-08-04 13:32:25 +0300

    try1

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,31 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SalaryInfo {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        Map<String, Integer> salaryMap = new HashMap<>();
+        for (String name : names) {
+            int fullSalary = 0;
+            LocalDate firstDate = LocalDate.parse(dateFrom, formatter);
+            LocalDate lustDate = LocalDate.parse(dateFrom, formatter);
+            for (String start : data) {
+                String[] parts = start.split(" ");
+                LocalDate date = LocalDate.parse(parts[0], formatter);
+                if (date.isAfter(firstDate) && date.isBefore(lustDate) && name.equals(parts[1])) {
+                    int hours = Integer.parseInt(parts[2]);
+                    int perHours = Integer.parseInt(parts[3]);
+                    fullSalary += hours * perHours;
+                }
+            }
+            salaryMap.put(name, fullSalary);
+        }
+        return salaryMap.toString();
     }
 }
